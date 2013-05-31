feb090231fb004d72de24826e5b2d05699435327
Christopher Stones <chris.stones@zoho.com>
2013-05-31 23:09:27 +0100

    Added android build file.

--- rh_texture_loader/Android.mk
+++ rh_texture_loader/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := rh_texture_loader
-LOCAL_SRC_FILES := rh_texture_loader.c
+LOCAL_SRC_FILES := rh_texture_loader.c lz4.c
 include $(BUILD_STATIC_LIBRARY)
 
 
